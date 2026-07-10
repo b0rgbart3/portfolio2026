e92e9d089fb0287d37a4dd31734d85fcb9e36e8f
Bart <b0rgbart3@gmail.com>
2026-07-10 10:01:54 -0700

    update server to use real ips

--- server/main.py
+++ server/main.py
@@ -21,6 +21,9 @@ def _get_client_ip(request: Request) -> str:
     forwarded = request.headers.get("X-Forwarded-For")
     if forwarded:
         return forwarded.split(",")[0].strip()
+    real_ip = request.headers.get("X-Real-IP")
+    if real_ip:
+        return real_ip
     return request.client.host if request.client else "unknown"
 
 
